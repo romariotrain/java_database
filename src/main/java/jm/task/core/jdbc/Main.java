67d274689782fff5013586b774885df1d79bce5e
roman <romamorozevich@yandex.com>
2024-05-28 17:45:49 +0300

    three-tier architecture

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,13 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        final UserService userService = new UserServiceImpl();
+        UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
+        final UserService userService = new UserServiceImpl(userDao);
         userService.createUsersTable();
         for (int i = 0; i < 4; i++) {
             userService.saveUser("name" + i, "lastname" + i, (byte) 25);
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,78 +1,41 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.exception.SQLGrammarException;
-import org.hibernate.query.Query;
-
-
+import lombok.AllArgsConstructor;
 import java.util.List;
 
+//@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserDaoHibernateImpl repository;
+
+    public UserServiceImpl(UserDaoHibernateImpl repository) {
+        this.repository = repository;
+    }
+
+
     public void createUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            // Таблица будет создана автоматически благодаря hbm2ddl.auto=update
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        repository.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            try {
-                String sql = "DROP TABLE Users";
-                session.createSQLQuery(sql).executeUpdate();
-            } catch (SQLGrammarException e) {
-
-                System.out.println("Таблицы Users не существует.");
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        repository.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            User user = new User(name, lastName, age);
-            session.persist(user);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        repository.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            User user = session.get(User.class, id);
-            if (user != null) {
-                session.delete(user);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        repository.removeUserById(id);
+
     }
 
     public List<User> getAllUsers() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Query<User> query = session.createQuery("FROM User", User.class);
-            return query.list();
-        }
+        return repository.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createQuery("DELETE FROM User").executeUpdate();
-            transaction.commit();
-        }
+        repository.cleanUsersTable();
     }
 }
